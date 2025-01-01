127dd7db81812b9932e2ae236ea9e27ec4b64806
aidan mcintosh <aidanamcintosh@gmail.com>
2025-01-01 13:31:11 -0400

    Changed DB connection code to fix db connection issues in prod and dev

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,11 +1,18 @@
 import { MongoClient } from "mongodb";
 const connectionString = process.env.ATLAS_URI || "";
-const client = new MongoClient(connectionString);
-let conn;
-try {
-	conn = await client.connect();
-} catch (e) {
-	console.error(e);
+const client = new MongoClient(connectionString, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+let db;
+async function connectToDatabase() {
+	try {
+		await client.connect();
+		db = client.db("ai_startup_ideator");
+		console.log("Successfully connected to the database");
+	} catch (e) {
+		console.error("Connection error", e);
+	}
 }
-let db = conn.db("ai_startup_ideator");
+connectToDatabase();
 export default db;
